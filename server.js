141afd65bd6d5b0c1429670fd3568c043cad8ec4
saikousik22 <144267372+saikousik22@users.noreply.github.com>
2025-05-20 17:41:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "kousik@1234", // Change to your MySQL password
+    password: "", // Change to your MySQL password
     database: "olabs",
 });
 
